eb41fb97df28012a582c114fc0789b2ea9b731ac
Jhonney <themyplace.10@gmail.com>
2025-03-31 21:02:47 -0300

    atualizações

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,7 +1,24 @@
 import S from "./doados.module.scss"
 import protagonistaLivro from "../../assets/protagonistaLivro.png"
-
+import { useEffect, useState } from "react";  
+import axios from 'axios'
 export default function doados(){
+
+
+    const [livros, setLivros] = useState([])
+    
+    const getLivros = async () => {
+        const response = await axios.get("https://projetoapi-ovgx.onrender.com/livros")
+        setLivros(response.data)
+    }
+
+    useEffect(()=>{
+        getLivros()
+    },[])
+
+
+
+
     return(
         <main className={S.mainDoados}>
             <section className={S.tituloLivrosDoados}>
@@ -17,6 +34,14 @@ export default function doados(){
                         <p className={S.genero}>Ficção</p>
                         </div>
                    </div>
+                   {livros.map((item) => (  
+                   <article className = {S.livrosInformacoes} key={item.id}>  
+                        <img src={item.imagem_url} alt={item.titulo} />  
+                        <h3 className={S.nomeFilme}> {item.titulo}</h3>  
+                        <p className={S.genero}>{item.autor}</p>  
+                        <p className ={S.autor}> {item.categoria}</p>  
+                   </article>  
+                  ))}  
                 </section>
             </section>
         </main>
--- src/Pages/QueroDoar/queroDoar.jsx
+++ src/Pages/QueroDoar/queroDoar.jsx
@@ -1,34 +1,95 @@
-import livroAberto from "../../assets/livroAberto.png"
-import S from "./queroDoar.module.scss"
+import React, { useState } from 'react';
+import axios from 'axios';
+import livroAberto from "../../assets/livroAberto.png";  
+import S from "./queroDoar.module.scss";  
 
-export default function queroDoar(){
-    return(
-        <main className={S.mainQueroDoar}>
-              <div className={S.preenchaInfoAds}>
-                <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
-              </div>
-              
-          <section className={S.boxPrincipal}>
+export default function QueroDoar() {  
+    const [titulo, setTitulo] = useState("");  
+    const [categoria, setCategoria] = useState("");  
+    const [autor, setAutor] = useState("");  
+    const [imagem_url, setImagem_url] = useState("");  
 
-              <form action= "" className={S.formularioBox}>
-               <div className={S.imgTxt}>
-                <img src={livroAberto} alt="imagem de um livro aberto" />
-                <h2>Informações do Livro</h2>
-               </div>
-               <section className={S.inputBox}>
-                <input type="text" placeholder="  Titulo"/>
-                <input type="text" placeholder="  Categoria"/>
-                <input type="text" placeholder="  Autor"/>
-                <input type="text" placeholder="  Link da Imagem"/>
-               </section>
-
-               <div className={S.btn}>
-                <button>Doar</button>
-               </div>
-              </form> 
-          
-          </section>
-
+    const enviarDados = async (e) => {    
+        e.preventDefault();
+        const urlApi = "https://projetoapi-ovgx.onrender.com/doar";  
         
-        </main>
-)}
+        const  dadosEnviar ={ 
+            titulo,  
+            categoria,  
+            autor,  
+            imagem_url  
+        };
+        
+        await axios.post(urlApi, dadosEnviar)
+
+        alert("Livro cadastrado!!")
+        // Limpa os campos após o envio
+        setTitulo("");
+        setCategoria("");
+        setAutor("");
+        setImagem_url("");
+    }
+    
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value)
+        console.log(e.target.value)
+    }
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+        
+    }
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+    const capturaImagem = (e) => {
+        setImagem_url(e.target.value)
+    }
+    
+
+    return (  
+        <main className={S.mainQueroDoar}>  
+            <div className={S.preenchaInfoAds}>  
+                <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>  
+            </div>  
+
+            <section className={S.boxPrincipal}>  
+                <form onSubmit={enviarDados} className={S.formularioBox}>  
+                    <div className={S.imgTxt}>  
+                        <img src={livroAberto} alt="imagem de um livro aberto" />  
+                        <h2>Informações do Livro</h2>  
+                    </div>  
+                    <section className={S.inputBox}>  
+                        <input 
+                            type="text" 
+                            placeholder="Título" 
+                            onChange={capturaTitulo} 
+                            value={titulo} 
+                        />  
+                        <input 
+                            type="text" 
+                            placeholder="Categoria" 
+                            onChange={capturaCategoria} 
+                            value={categoria} 
+                        />  
+                        <input 
+                            type="text" 
+                            placeholder="Autor" 
+                            onChange={capturaAutor} 
+                            value={autor} 
+                        />  
+                        <input 
+                            type="text" 
+                            placeholder="Link da Imagem" 
+                            onChange={capturaImagem} 
+                            value={imagem_url} 
+                        />  
+                    </section>  
+
+                    <div className={S.btn}>  
+                        <button type="submit" value = "Doar" onClick = {enviarDados}>Doar</button>  
+                    </div>  
+                </form>  
+            </section>  
+        </main>  
+    );   
+}
